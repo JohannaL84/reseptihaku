9f8d32d379d40a260de3152cf5bc7e2e2bf9b632
Marjatta Wallinheimo <marjatta.wallinheimo@gmail.com>
2025-02-10 11:06:42 +0200

    Lisätty jaa-linkki hakutuloksiin

--- search.js
+++ search.js
@@ -64,16 +64,43 @@ document.addEventListener('DOMContentLoaded', function() {
                             <h3>${recipe.name || recipe.title}</h3>
                             <p>${recipe.description}</p>
                             <a href="${recipe.link}" target="${recipe.external ? '_blank' : '_self'}" class="recipe-link">Katso resepti</a>
-                            ${loggedInUser.username ? `<span class="heart-icon" data-recipe="${recipe.name || recipe.title}">&#10084;</span>` : ''}
+                            ${loggedInUser.username ? `
+                                <span class="heart-icon" data-recipe="${recipe.name || recipe.title}">&#10084;</span>
+                                <a href="#" class="share-link" data-recipe="${recipe.name || recipe.title}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-share" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+                                        <path d="M14 9V5l7 7-7 7v-4H5V9h9zm-9 0H3v6h2V9z"/>
+                                    </svg>
+                                    Jaa resepti
+                                </a>
+                            ` : ''}
                         `;
 
                         resultsContainer.appendChild(recipeElement);
                     });
 
+                    // Lisätään tapahtumankuuntelijat suosikeille ja jakamiselle
                     document.querySelectorAll('.heart-icon').forEach(icon => {
                         icon.addEventListener('click', function() {
                             const recipeName = icon.getAttribute('data-recipe');
                             toggleFavoriteRecipe(icon, recipeName);
+                        });
+                    });
+
+                    document.querySelectorAll('.share-link').forEach(link => {
+                        link.addEventListener('click', function(event) {
+                            event.preventDefault();
+                            const recipeName = link.getAttribute('data-recipe');
+                            const shareUrl = `${window.location.origin}/resepti.html?name=${encodeURIComponent(recipeName)}`;
+
+                            // Kopioidaan linkki leikepöydälle
+                            navigator.clipboard.writeText(shareUrl)
+                                .then(() => {
+                                    alert('Reseptin linkki kopioitu leikepöydälle!');
+                                })
+                                .catch(err => {
+                                    console.error('Linkin kopiointi epäonnistui:', err);
+                                    alert('Linkin kopiointi epäonnistui. Yritä uudelleen.');
+                                });
                         });
                     });
                 }
